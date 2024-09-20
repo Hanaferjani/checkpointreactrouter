0a27e089df812178fd087988dc1f116cf2ccc0ef
Hanaferjani <Hene.ferjani@gmail.com>
2024-09-20 18:10:04 +0200

    Add files via upload

---
+++ src/components/Data.js
@@ -0,0 +1,31 @@
+const Movies = [
+    {
+        title: "the union",
+        poster: "https://fr.web.img2.acsta.net/img/1a/cf/1acf15b487da4c73d0bca4cc3a80e971.jpg",
+        rating: 5,
+        actors: ["Mark Wahlberg", "Halle Berry"],
+        describe: "Mike McKenna is a construction worker living in New Jersey . One night in a bar, he meets Roxanne Hall, his high school girlfriend. She will introduce him to the world of spies and make him work for the government.",
+        trailer: "https://www.youtube.com/embed/vea9SdnRMyg?si=uHzOZ1RJFxTtRPkj",
+        id:1
+    },
+    {
+        title: "rebel ridge",
+        poster: "https://dnm.nflximg.net/api/v6/mAcAr9TxZIVbINe88xb3Teg5_OA/AAAABaSWW48t0IluMnNMUonTWQgWcAlj8wyBOu2HYjiNTyy406d8vAGtfcUMq2M0qMGVSng0NUQQn5rP4QCE_ElHCt6M3F62W05Ksc7-Mo1OGv4rcu1vGBk9HaL5HPUR-AVB8MZXVg.jpg?r=488",
+        rating: 4.5,
+        actors: ["Aaron Pierre", "AnnaSophia Robb"],
+        describe: "Terry Richmond, un ancien Marine, se rend dans la ville de Shelby Springs pour y déposer la caution pour faire libérer son cousin. Mais toutes les économies de Terry sont injustement saisies par la police locale dirigée par le chef corrompu, Sandy Burnne. Terry va cependant pouvoir compter sur l'aide de la greffière Summer McBride. Ils vont mettre à jour une vaste conspiration généralisée au sein de Shelby Springs. Terry va tout tenter pour récupérer l'argent de la caution.",
+        trailer: "https://www.youtube.com/embed/gF3gZicntIw?si=4Xun3h8Qa4QNWmpH",
+        id:2
+    },
+    {
+
+        title: "incoming",
+        poster: "https://m.media-amazon.com/images/M/MV5BNTkyNzA5YmMtZjE3ZC00YjljLThkNDQtZGI0NWEyZmZkZTNlXkEyXkFqcGc@._V1_.jpg",
+        rating: 3.5,
+        actors: ["Mason Thames", "Isabella Ferreira"],
+        describe: "Four new students face the terrors of adolescence at their very first high school party.",
+        trailer: "https://www.youtube.com/embed/elA7XuEUxOo?si=MpayAqM6IPHq-PWQ",
+        id:3
+    }
+]
+export default Movies
